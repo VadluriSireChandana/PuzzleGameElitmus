8e800b31f6a747b94d955d29c113dde74e0817d6
VadluriSireChandana <105452010+VadluriSireChandana@users.noreply.github.com>
2023-05-08 23:38:31 +0530

    Add files via upload

---
+++ puzzhtml/options/quiz/js/questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What is the name of the yellow, crescent-shaped object that appears in the sky at night?",
+    answer: "moon",
+    options: [
+      "sun",
+      "ball",
+      "moon",
+      "venus"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What is the name of the planet that we live on?  ",
+    answer: "Earth",
+    options: [
+      "Sun",
+      "Earth",
+      "Venus",
+      "Mars"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Who was the first person to step on the moon?",
+    answer: "Neil Armstrong",
+    options: [
+      "Neil Armstrong",
+      "Me",
+      "My Mom",
+      "Modi"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What is the capital city of France?",
+    answer: "Paris",
+    options: [
+      "Paris",
+      "India",
+      "America",
+      "Arabia"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What is the name of the famous building in Paris with a tall, pointy top? ",
+    answer: "Eiffel Tower",
+    options: [
+      "Current  Tower",
+      "Twin tower",
+      "Taj Mahal ",
+      "Eiffel Tower"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
